8467fee6eccb5fe8bd9dd89d63e6c1fad246fa6e
zhangyiming <zhangyiming07@163.com>
2016-11-09 00:18:41 +0800

    UDP连接实现

---
+++ socket建立/UDPServ.py
@@ -0,0 +1,22 @@
+# -*- encoding:utf-8 -*-
+__author__ = 'zhangym'
+
+import socket
+
+HOST = ''
+PORT = 3214
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+s.bind((HOST,PORT))
+
+data = True
+
+while data:
+    data,addr = s.recvfrom(1024)
+    if data == b'bye':
+        break
+    print('Recieve String:',data.decode('utf-8'))
+    s.sendto(data,addr)
+
+s.close()
